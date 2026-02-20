cbbeb33e3fbbce2fd0d2f636a78b348f05d440bb
Ridham Sawhney <60002757+ridham-sawhney@users.noreply.github.com>
2026-02-21 00:20:10 +0530

    Implement Kadane's Algorithm for maximum subarray sum in cpp

---
+++ 3_KadanesAlgo.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int maxSum = INT_MIN;
+        int tempSum = 0;
+        for(int i=0;i<nums.size();i++){
+            tempSum += nums[i];
+            if(tempSum > maxSum){
+                maxSum = tempSum;
+            }
+            if(tempSum<0){
+                tempSum = 0;
+            }
+        }
+        return maxSum;
+    }
+};
